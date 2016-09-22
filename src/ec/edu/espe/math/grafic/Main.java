dd37c0c53a3858782d45205ba898dbf577f4db79
Labs-DECC <Labs-DECC@10.9.5.40>
2016-09-22 10:39:31 -0500

    Geometric & Graphic

---
+++ src/ec/edu/espe/math/grafic/Main.java
@@ -0,0 +1,19 @@
+package ec.edu.espe.math.grafic;
+import ec.edu.espe.math.grafic.forms.LoginScreens;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Labs-DECC
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoginScreens obj = new LoginScreens();
+        obj.show();
+    }
+}
